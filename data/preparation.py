db6fb59080b1e558acaa0514c233a6284f27cc40
FERHAT AKYÜZ <ferhatakyuz151@icloud.com>
2026-04-09 13:03:54 +0300

    Update preparation.py

--- data/preparation.py
+++ data/preparation.py
@@ -36,3 +36,19 @@ test_ds = create_split('test')
 print(f"Eğitim seti boyutu: {len(train_ds)}")
 ├── requirements.txt    # requests, datasets, json kütüphanelerini buraya yaz
 └── README.md           # Projenin "vitrini"
+# 1. Anlaşmazlık olanları temizle yani sadece çelişki olmayan veriler tutulur.
+train_ds_clean = train_ds.filter(lambda x: not check_true_disagreement(x))
+
+print(f"Temizlik sonrası train seti boyutu: {len(train_ds_clean)}")
+# 2. Nefret söylemi olup gerekçesi (rationale) olmayanları temizle
+def has_rationales(example):
+    labels = [ann["label"] for ann in example["annotators"]]
+    majority = max(set(labels), key=labels.count)
+    if majority in ["hatespeech", "offensive"]:
+        # Rationale listesi boşsa veya sadece 0'lardan oluşuyorsa False döner
+        if not example["rationales"] or sum([sum(r) for r in example["rationales"]]) == 0:
+            return False
+    return True
+
+train_ds_clean = train_ds_clean.filter(has_rationales)
+print(f"Temizlik sonrası train seti boyutu: {len(train_ds_clean)}")
