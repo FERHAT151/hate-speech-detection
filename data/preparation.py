57a1eed2fda3e5f8328d7cbbda78d98dd163f0a8
FERHAT AKYÜZ <ferhatakyuz151@icloud.com>
2026-04-09 13:07:17 +0300

    Update preparation.py

--- data/preparation.py
+++ data/preparation.py
@@ -36,11 +36,11 @@ test_ds = create_split('test')
 print(f"Eğitim seti boyutu: {len(train_ds)}")
 ├── requirements.txt    # requests, datasets, json kütüphanelerini buraya yaz
 └── README.md           # Projenin "vitrini"
-# 1. Anlaşmazlık olanları temizle yani sadece çelişki olmayan veriler tutulur.
+# 4. Anlaşmazlık olanları temizle yani sadece çelişki olmayan veriler tutulur.
 train_ds_clean = train_ds.filter(lambda x: not check_true_disagreement(x))
 
 print(f"Temizlik sonrası train seti boyutu: {len(train_ds_clean)}")
-# 2. Nefret söylemi olup gerekçesi (rationale) olmayanları temizle
+# 5. Nefret söylemi olup gerekçesi (rationale) olmayanları temizle
 def has_rationales(example):
     labels = [ann["label"] for ann in example["annotators"]]
     majority = max(set(labels), key=labels.count)
